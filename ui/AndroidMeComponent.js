ef5d7c03235ea178789177e085a1a1db6751185c
okpalaChidiebere <okpalacollins4@gmail.com>
2021-06-21 17:54:31 -0600

    Respnding to clicks
    - In the BodyPartFragment we set an onclick listener on the Image and onclick we increment the list index and set the image resource
    - if we reach the end of the list we set the list index back to 0 (first item on the list)
    - We create a react state to help us manage and store the state of the index and list of images
    
    FYI:
    - We did not need to worry about saving the state when the device rotates like we would normally do with vanilla android. ReactNative handles that for us :) YEY!
    - I added ScrollView in AndroidMeComponent so that we will maitain thesame clean size of page when i potrait mode on landscape mode as well! This was a good learn

--- ui/AndroidMeComponent.js
+++ ui/AndroidMeComponent.js
@@ -1,21 +1,23 @@
 import React from "react"
-import { View, StyleSheet }  from "react-native"
+import { View, StyleSheet, ScrollView }  from "react-native"
 import BodyPartFragment from "./BodyPartFragment"
 import { getHeads, getBodies, getLegs } from "../utils/AndroidImageAssets"
 
 export default function AndroidMeComponent() {
-    return ( 
-    <View style={styles.container}>
-      <View style={styles.frames}>
-        <BodyPartFragment imageIds={getHeads()} listIndex={1}/>
-      </View>
-      <View style={styles.frames}>
-        <BodyPartFragment imageIds={getBodies()} listIndex={1}/>
-      </View>
-      <View style={styles.frames}>
-        <BodyPartFragment imageIds={getLegs()} listIndex={1}/>
-      </View>
-    </View>
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.frames}>
+            <BodyPartFragment imageIds={getHeads()} listIndex={1}/>
+          </View>
+          <View style={styles.frames}>
+            <BodyPartFragment imageIds={getBodies()} listIndex={1}/>
+          </View>
+          <View style={styles.frames}>
+            <BodyPartFragment imageIds={getLegs()} listIndex={1}/>
+          </View>
+        </View>
+      </ScrollView>
     )
 }
 
@@ -25,11 +27,11 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
-      padding: 32,
     },
     frames: {
       flex: 1,
       flexGrow: 1,
+      padding: 5,
     }
   })
 
--- ui/BodyPartFragment.js
+++ ui/BodyPartFragment.js
@@ -1,13 +1,40 @@
-import React, { Fragment } from "react"
-import { Image }  from "react-native"
+import React, { Fragment, useState, useEffect } from "react"
+import { Image, Pressable }  from "react-native"
 
 export default function BodyPartFragment({ listIndex, imageIds }){
+
+    const [ state, setState ] = useState({
+        mImageIds: [],
+        mListIndex: 0,
+      })
+    
+    useEffect(() => {
+
+        setState({
+            mImageIds: imageIds,
+            mListIndex: listIndex,
+        }) 
+    }, [ imageIds,listIndex ]) //We set the dependencies that will control this component. Anytime the "Index to display first" changes, we re-render the whole image
+
+    // Set a click listener on the Image
+    const onClick = () => {
+        setState((curr) => ({
+            ...curr, 
+            //if we reached the end of the index and the user clicks again, we reset the index to the begining of the list
+            mListIndex: curr.mListIndex < curr.mImageIds.length-1 ? ++curr.mListIndex : 0,
+        }))
+    }
+
+    const { mImageIds, mListIndex } = state
+
     return (
         <Fragment>
-            <Image 
-            style={{flex: 1}}
-            source={imageIds[listIndex]}
-            />
+            <Pressable onPress={onClick}>
+                <Image 
+                style={{flex: 1}}
+                source={/* Set the image resource to the list item at the stored index */ mImageIds[mListIndex]}
+                />
+            </Pressable>
         </Fragment>
     )
 }
